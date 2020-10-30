bb03c3f822d130bb881d622ac8430b3edeeececd
igmrrf <francis.igbiriki.fi@gmail.com>
2020-10-31 00:59:40 +0100

    Fixing market Dropdown

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,9 +4,59 @@ import logo from '../../static/img/logo.svg';
 import mobileLogo from '../../static/img/mobile-logo.svg';
 import close from '../../static/icon/close.png';
 import menu from '../../static/icon/menu.png';
+import retail from '../../static/img/retail.png';
+import medicine from '../../static/img/medicine.png';
+import parts from '../../static/img/engineering.png';
 import './header.css';
 
-const Menu = ({ setOpen }) => (
+const MobileDropDown = () => (
+  <div className='market-dropdown' id='mobile-market-dropdown'>
+    <div className='center'>
+      <NavLink to='/fashion' className='col'>
+        <img src={retail} alt='' width='100vw' style={{ margin: '2px' }} />
+        <p>Retail/Luxury</p>
+      </NavLink>
+      <NavLink to='/pharmaceutical' className='col'>
+        <img src={medicine} alt='' width='100vw' style={{ margin: '2px' }} />
+        <p>Pharmaceutical</p>
+      </NavLink>
+      <NavLink to='engineering' className='col'>
+        <img src={parts} alt='' width='100vw' style={{ margin: '2px' }} />
+        <p>Engineering</p>
+      </NavLink>
+    </div>
+  </div>
+);
+const DesktopDrown = ({ dropped, setDropped }) => (
+  <div className='container' style={{ background: 'white' }}>
+    <div
+      className='row market-dropdown'
+      id='desktop-market-dropdown'
+      onClick={() => setDropped(!dropped)}
+    >
+      <NavLink to='fashion' className='col-3 item'>
+        <img src={retail} alt='' />
+        <h4>Retail/Luxury</h4>
+        <p>Unique Consumer Experiences Means Better Brand Outlook.</p>
+      </NavLink>
+      <NavLink to='pharmaceutical' className='col-3 item'>
+        <img src={medicine} alt='' />
+        <h4>Pharmaceutical</h4>
+        <p>Verifiable Products Means Better Consumer Safety.</p>
+      </NavLink>
+      <NavLink to='engineering' className='col-3 item'>
+        <img src={parts} alt='' />
+        <h4>Engineering</h4>
+        <p>
+          From easier access to technical information to better production
+          lead-time.
+        </p>
+      </NavLink>
+    </div>
+  </div>
+);
+
+const Menu = ({ setOpen, dropped, setDropped }) => (
   <div className='menu'>
     <div className='brand'>
       <Link className='menu-logo' href='/'>
@@ -32,9 +82,15 @@ const Menu = ({ setOpen }) => (
       <NavLink to={'/features'} className='menu-item'>
         Features
       </NavLink>
-      <NavLink to={'/about'} className='menu-item'>
+      <div
+        to={'/about'}
+        className='menu-item'
+        id='dropdown'
+        onClick={() => setDropped(!dropped)}
+      >
         Market <span className='caret'></span>
-      </NavLink>
+      </div>
+      {dropped ? <MobileDropDown /> : null}
       <NavLink to={'/blog'} className='menu-item'>
         Blog
       </NavLink>
@@ -47,6 +103,7 @@ export default function Header() {
   const [scrollState, setScrollState] = useState('top');
   const [bottom, setBottom] = useState(false);
   const [open, setOpen] = useState(false);
+  const [dropped, setDropped] = useState(false);
 
   useEffect(() => {
     listener = document.addEventListener('scroll', (e) => {
@@ -77,7 +134,9 @@ export default function Header() {
         bottom ? 'header-bottom' : ''
       } header`}
     >
-      {open ? <Menu setOpen={setOpen} /> : null}
+      {open ? (
+        <Menu setOpen={setOpen} dropped={dropped} setDropped={setDropped} />
+      ) : null}
 
       <div className='container navigation'>
         <button
@@ -107,7 +166,11 @@ export default function Header() {
           <NavLink to={'/features'} className='nav-item'>
             Features
           </NavLink>
-          <NavLink to={'/about'} className='nav-item'>
+          <NavLink
+            to={window.location.pathname}
+            onClick={() => setDropped(!dropped)}
+            className='nav-item'
+          >
             Market <span className='caret'></span>
           </NavLink>
           <NavLink to={'/blog'} className='nav-item'>
@@ -118,6 +181,9 @@ export default function Header() {
           Contact us
         </button>
       </div>
+      {dropped ? (
+        <DesktopDrown dropped={dropped} setDropped={setDropped} />
+      ) : null}
     </div>
   );
 }
